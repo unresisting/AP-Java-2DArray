bf29e06a78ecc7ddae2885a87ad6a523ae7ac2c6
Andy <info@aguo.dev>
2021-10-17 21:45:55 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Main {
+  public static void main(String[] args)
+	{
+		TwoDArray buttonGui= new TwoDArray();
+		buttonGui.setVisible(true);
+    
+	}
+}
